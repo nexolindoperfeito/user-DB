a0d1759c95f5754e450c2a1ef9317d6933e72fe9
= <=>
2026-06-13 17:20:30 -0300

    implementacao swagger

---
+++ src/doc/swagger.js
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import swaggerAutogen from "swagger-autogen";
+
+const doc = {
+    info : {
+        title: 'minha API',
+        description: "Uma simples API para gerenciamento de usuários"
+    },
+    host: 'localhost: ' + process.env.SERVER_PORT,
+    scremes: ['http']
+};
+
+const outputfile = './swagger-output.json';
+const routes = ['./src/server.js'];
+swaggerAutogen()(outputfile, routes, doc);
